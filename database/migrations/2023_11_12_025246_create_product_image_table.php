d7229681e6ce9d0fd0ca0a817e52db4ca8efc9df
NguyenVanPhuoc1 <nguyenvanphuoc031123@gmail.com>
2023-11-12 12:10:28 +0700

    phuoc create database v1

---
+++ database/migrations/2023_11_12_025246_create_product_image_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->id();
+            $table->string('pro_image');//ảnh
+            $table->integer('pro_id');//id sản phẩm
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_image');
+    }
+};
